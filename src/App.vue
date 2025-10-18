a56c6b3a97af59044053f062198bd6d51e4cec7b
陆玉莲 <827771494@qq.com>
2025-10-18 14:36:25 +0800

    初始代码

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+onMounted(() => {
+  let testNum = null
+  if (testNum.length > 1) {
+    console.log('报错了')
+  }
+})
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
